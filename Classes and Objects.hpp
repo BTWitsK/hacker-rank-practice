5a3ca5412a65d5e449a04d1da118bb9b719ec376
BTWitsK <K@Wakanda.local>
2017-06-26 22:20:16 -0500

    classes and objects.cpp

---
+++ Classes and Objects.hpp
@@ -0,0 +1,14 @@
+//
+//  Classes and Objects.hpp
+//  
+//
+//  Created by BTWitsK on 6/26/17.
+//
+//
+
+#ifndef Classes_and_Objects_hpp
+#define Classes_and_Objects_hpp
+
+#include <stdio.h>
+
+#endif /* Classes_and_Objects_hpp */
---
+++ Strings.hpp
@@ -0,0 +1,14 @@
+//
+//  Strings.hpp
+//  Hacker Rank Practice
+//
+//  Created by BTWitsK on 6/25/17.
+//  Copyright © 2017 K. All rights reserved.
+//
+
+#ifndef Strings_hpp
+#define Strings_hpp
+
+#include <stdio.h>
+
+#endif /* Strings_hpp */
