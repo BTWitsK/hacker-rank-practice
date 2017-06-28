17064b593157ed014603c4e85844195c7b5a8f11
BTWitsK <K@Wakanda.local>
2017-06-28 01:22:09 -0500

    updated folders added xcode project

---
+++ HackerRank/C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HackerRank
+//
+//  Created by BTWitsK on 6/28/17.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
