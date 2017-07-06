49dc090a5656543a3e3f06067c1e5dfed02420c2
K <Btwitsk@gmail.com>
2017-07-06 15:53:41 -0500

    updated Preprocessor Solution.cpp

--- HackerRank/C++/Other Concepts/Preprocessor Solution.cpp
+++ HackerRank/C++/Other Concepts/Preprocessor Solution.cpp
@@ -6,7 +6,7 @@
 //
 //
 
-#include <stdio.h>
+
 
 #include <iostream>
 #include <vector>
@@ -17,30 +17,38 @@ using namespace std;
 #endif
 
 
-#define toStr(a) " "
-#define foreach(a,b) for (auto b: a)
-#define io(a) v
-#define FUNCTION ()
+#define toStr(x) #x
+#define foreach(x,i) for(int i = 0; i < x.size(); i++)
+#define INF = 80000
+#define -INF = -80000
+  
+#define io(a)[i] int x = 0; cin >> x; a[i] = x;
 
 
-FUNCTION(minimum, <)
-FUNCTION(maximum, >)
+#define FUNCTION(minimum, <) void minimum(int v, int &mn){(for int i = 0; i < v.size(); i++){ if (mn > v[i]) mn = v[i]; }}
+#define FUNCTION(maximum, >) void maximum(int v, int &mx){(for int i = 0; i < v.size(); i++){ if (mx < v[i]) mx = v[i]; }}
+#define minimum(mn, v[i]) minimum(v,mx)
+#define maximum(mx, v[i]) maximum
+
+FUNCTION(minimum, <) //macro for function definition (done)
+FUNCTION(maximum, >) //macro for function definition (done)
 
 int main(){
     int n; cin >> n;
     vector<int> v(n);
-    foreach(v, i) {
-        io(v)[i];
+    foreach(v, i) { //for(int i = 0; i < v.size(); i++)
+        io(v)[i]; // int x; cin >> x; a[i] = x;
+                
     }
-    int mn = INF;
+    int mn = INF; // macro for INF & -INF
     int mx = -INF;
-    foreach(v, i) {
-        minimum(mn, v[i]);
-        maximum(mx, v[i]);
+    foreach(v, i) { //for(int i = 0; i < v.size(); i++)
+        minimum(mn, v[i]); // macro for function call
+        maximum(mx, v[i]); // macro for function call 
     }
     int ans = mx - mn;
   
-    cout << toStr(Result =) <<' '<< ans;
+    cout << toStr(Result =) <<' '<< ans; // toStr(x) #x
     return 0;
     
 }
