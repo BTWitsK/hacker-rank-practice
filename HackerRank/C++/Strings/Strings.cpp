a71fd58ead07d5147591b249e461dc8fdc3e7b72
BTWitsK <BTWitsK@Gmail.com>
2017-07-09 23:19:21 -0500

    refactored strings.cpp

--- HackerRank/C++/Strings/Strings.cpp
+++ HackerRank/C++/Strings/Strings.cpp
@@ -2,6 +2,17 @@
 //  Strings.cpp
 //  Hacker Rank Practice
 //
+//  Input Format
+//  You are given two strings, a and b, separated by a new line. Each string will consist of lower case Latin characters ('a'-'z').
+//
+//  Output Format
+//
+// In the first line print two space-separated integers, representing the length of a and b respectively.
+//
+// In the second line print the string produced by concatenating a and b (a+b).
+//
+// In the third line print two strings separated by a space,a'  and b'. a' and b' are the same as a and b, respectively, except that their first characters are swapped.
+//
 //  Created by BTWitsK on 6/25/17.
 //  Copyright © 2017 K. All rights reserved.
 //
@@ -13,24 +24,27 @@ using namespace std;
 int main() {
     string FirstString, SecondString;
     
+//get strings from input
     
     cin >> FirstString >> SecondString;
-    int FirstStringSize = FirstString.size();
-    int SecondStringSize = SecondString.size();
+    
+//get string size and concatenate strings
+    auto FirstStringSize = FirstString.size();
+    auto SecondStringSize = SecondString.size();
     string ComboString = FirstString + SecondString;
     
+//display string size and concatenated strings
+    cout << FirstStringSize << " " << SecondStringSize;
+    cout << endl << ComboString << endl;
     
-    
-    
-    cout << FirstStringSize << " " << SecondStringSize << endl;
-    cout << ComboString << endl;
-    
+//swap first character from each string
     char SwapOne = FirstString[0];
     char SwapTwo = SecondString[0];
     
     FirstString[0] = SwapTwo;
     SecondString[0] = SwapOne;
     
+//display strings after first character has been swapped
     cout << FirstString << " " << SecondString;
     return 0;
 }
